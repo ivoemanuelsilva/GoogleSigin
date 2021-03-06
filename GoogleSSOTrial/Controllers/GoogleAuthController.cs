2bb34f63d678ec5190851308c431434c7a70a85e
ivoemanuelsilva <79471730+ivoemanuelsilva@users.noreply.github.com>
2021-03-06 16:00:02 +0000

    Google SignIn

---
+++ GoogleSSOTrial/Controllers/GoogleAuthController.cs
@@ -0,0 +1,39 @@
+﻿using Google.Apis.Auth;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace GoogleSSOTrial.Controllers
+{
+    [RequireHttps]
+    public class GoogleAuthController : Controller
+    {
+
+        private readonly ILogger<GoogleAuthController> _logger;
+
+        public GoogleAuthController(ILogger<GoogleAuthController> logger)
+        {
+            this._logger = logger;
+        }
+
+
+        [HttpPost]
+        public IActionResult SignInToken([FromBody] PostModel postModel)
+        {
+            GoogleJsonWebSignature.Payload payload = GoogleJsonWebSignature.ValidateAsync(postModel.SignInToken).GetAwaiter().GetResult();
+
+            return Json(payload);
+        }
+    }
+
+
+    public class PostModel
+    {
+        public string SignInToken { get; set; }
+    }
+
+}
---
+++ GoogleSSOTrial/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GoogleSSOTrial.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
